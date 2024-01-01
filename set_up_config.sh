e74cc45cb28791f5f4301b2035b396516e90994e
Las02 <97394529+Las02@users.noreply.github.com>
2024-01-01 23:45:02 +0100

    Update set_up_config.sh

--- set_up_config.sh
+++ set_up_config.sh
@@ -1,6 +1,15 @@
 
-# append to bascrc the preconfigured bashrc
-cat bashrcmain >> .bashrc
+# Make copy of .config and .tmux.conf
+mv ~/.config ~/.config.bck
+mv ~/.tmux.conf ~/.tmux.conf.bck
+
+# And move mine in
+mv .config ~/.config
+mv .tmux.conf ~/.tmux.conf
+
+# append to bascrc the preconfigured bashrc 
+echo "---- ADDED -----" >> ~/.bashrc
+cat bashrcmain >> ~/.bashrc
 
 # make nvim appimage
 mkddir -p ~/bin
@@ -10,7 +19,7 @@ chmod u+x nvim.appimage
 cd ..
 
 # add to path the nvim appimage
-echo "export PATH=$PATH:~/bin" >> .bashrc
+echo "export PATH=$PATH:~/bin" >> ~/.bashrc
 
 
 # Install nvchad 
@@ -18,5 +27,4 @@ git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 
 # and add the custom config to nvim
 git clone https://github.com/Las02/nvimconfig ~/.config/nvim/lua
-
 
