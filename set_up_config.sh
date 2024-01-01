20470004c06f1d45898d9786701646ff70a7d5e6
las02 <lasse101010@gmail.com>
2024-01-01 23:32:01 +0100

    first

---
+++ set_up_config.sh
@@ -0,0 +1,20 @@
+
+# append to bascrc the preconfigured basrch
+cat bashrcmain >> .bashrc
+
+# make nvim appimage
+mkddir -p ~/bin
+cd ~/bin
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+cd ..
+
+# add to path the nvim appimage
+echo "export PATH=$PATH:~/bin" >> .bashrc
+
+
+# Install nvchad 
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+
+# and add the custom config to nvim
+
